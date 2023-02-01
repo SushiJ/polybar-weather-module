0d31a24b7a3313d65da9828213bf2f8964e0ea03
Sushij <sushant.wurk@gmail.com>
2023-02-01 12:27:17 +0530

    Feat. Icon changing on type of day & add humidity icon

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #!/bin/env python3
-import requests
 from math import ceil
+from datetime import datetime
+import requests
+
 from utils.io_helper import get_data
 
 
@@ -9,14 +11,26 @@ CITY = ''
 BASE = "https://api.openweathermap.org/data/2.5/weather"
 UNITS = "metric"
 ICONS = {
-    "Clear": '',
-    "Clouds": '',
-    "Rain": '',
-    "Thunderstorm": '',
-    "Snow": '',
-    "Drizzle": '',
-    "Mist": '',
-    "Haze": ''
+    "SUN": {
+        "Clear": '',
+        "Clouds": '',
+        "Rain": '',
+        "Thunderstorm": '',
+        "Snow": '',
+        "Drizzle": '',
+        "Mist": '',
+        "Haze": ''
+    },
+    "MOON": {
+        "Clear": '',
+        "Clouds": '',
+        "Rain": '',
+        "Thunderstorm": '',
+        "Snow": '',
+        "Drizzle": '',
+        "Mist": '',
+        "Haze": ''
+    }
 }
 
 
@@ -41,19 +55,30 @@ def get_humidity(json):
     return json["main"]["humidity"]
 
 
+def type_of_icon():
+    curr_time = datetime.now()
+    list = str(curr_time.time()).split(':')[:2]
+    Str = ''.join(list)
+    if 630 < int(Str) < 1830:
+        return "SUN"
+    else:
+        return "MOON"
+
+
+def get_icon(json):
+    icon_type = type_of_icon()
+    condition = json["weather"][0]["main"]
+    icon = ICONS[icon_type][condition]
+    return icon
+
+
 def request_api_data():
     json_data = get_data_from_api()
     temp_data = get_temp_data(json_data)
     icon = get_icon(json_data)
     humidity = get_humidity(json_data)
-    format = f"{temp_data} | {icon}  | {humidity}"
+    format = f"{icon}  | {temp_data} | {humidity}% "
     print(format)
-
-
-def get_icon(json):
-    condition = json["weather"][0]["main"]
-    icon = ICONS[condition]
-    return icon
 
 
 if __name__ == "__main__":
